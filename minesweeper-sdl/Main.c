23f0b07ad85c813d610f571016afbeb410654075
evⁿ <gh@cli.lol>
2023-02-27 13:39:34 +0100

    Initialization!

--- minesweeper-sdl/Main.c
+++ minesweeper-sdl/Main.c
@@ -1,7 +1,17 @@
 #include <SDL.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		printf("SDL initialization failed. SDL Error: %s", SDL_GetError());
+	}
+	else
+	{
+		printf("SDL initialization succeeded!");
+	}
+
+	(void)getchar();
     return 0;
 }
